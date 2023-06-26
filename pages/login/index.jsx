d86e4da1cf3c6b1c2a7d0999e45cc882a8b2015a
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-06-26 12:15:11 +0200

    clean  pages/login/index.jsx

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,36 +1,34 @@
-import Heading from "@components/heading";
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react' //finns användare, och info om användare
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import Heading from "@components/heading";
 
 const LoginPage = () => {
   const supabaseClient = useSupabaseClient()
   const user = useUser();
   console.log(user);
-  const router = useRouter(); //hanterar navigering i projekt
-  // console.log("router", router);
+  const router = useRouter();
 
   useEffect(() => {
-    //om inloggad, skicka till den här URL:en (med router.push())
     if (user) {
       router.push("/");
     }
   }, [user, router])
 
-    return (
-      <>
+  return (
+    <>
       <Heading>Login</Heading>
       <Auth
-        redirectTo="http://localhost:3000/" //kopplad till providers, inte "denhär". Vi kommer inte till den här adressen vid email inloggning.  anv useRouter
+        redirectTo="http://localhost:3000/"
         appearance={{ theme: ThemeSupa }}
-        supabaseClient={supabaseClient} //från _app.tsx
+        supabaseClient={supabaseClient}
         providers={['google', 'github']}
         socialLayout="horizontal"
       />
-      </>
-    )
+    </>
+  )
 }
 
 export default LoginPage
