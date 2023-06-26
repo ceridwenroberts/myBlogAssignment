793965578d3676cd251df7285c07fa6b6c13d697
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-06-26 12:01:28 +0200

    pages/blog/[slug]/index.jsx

--- pages/blog/[slug]/index.jsx
+++ pages/blog/[slug]/index.jsx
@@ -1,4 +1,10 @@
 import { useRouter } from "next/router";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import useSWR from 'swr';
+import useSWRMutation from "swr/mutation";
+
+import { getPost, deletePost, postsCacheKey } from "../../../api-routes/posts";
+
 import styles from "./blog-post.module.css";
 import Comments from "./partials/comments";
 import AddComment from "./partials/add-comment";
@@ -6,43 +12,30 @@ import Button from "@components/button";
 import Heading from "@components/heading";
 import BlogImageBanner from "@components/blog-image-banner";
 
-import { getPost, deletePost, postsCacheKey } from "../../../api-routes/posts";
-import useSWR from 'swr';
-import useSWRMutation from "swr/mutation";
-import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 
 
 export default function BlogPost() {
-
   const router = useRouter();
   const { slug } = router.query;
   const user = useUser();
 
   const { trigger: deletePostTrigger } = useSWRMutation(postsCacheKey, deletePost);
 
-    const {
-      data: { data: post = {} } = {},
-      error
-    } = useSWR(slug ? `${postsCacheKey}${slug}` : null, () => getPost({ slug }));
-  // console.log("slugBlog", { data, error });
-  // console.log({ slug });
-  // console.log("postsData:", post);
+  const {
+    data: { data: post = {} } = {},
+    error
+  } = useSWR(slug ? `${postsCacheKey}${slug}` : null, () => getPost({ slug }));
 
+  const isAuthor = post?.user_id === user?.id;
 
-    const isAuthor = post?.user_id === user?.id;
-      if (isAuthor) console.log("isAuthor true");
-  
   const handleEditPost = () => {
-    console.log("slug", slug);
     router.push(`/blog/${slug}/edit`);
   };
-
 
   const handleDeletePost = async () => {
     await deletePostTrigger({
       id: post.id,
-    })
-    console.log("handleDeletePost (in [slug]/index)", { id: post.id, slug });
+    });
 
     if (!post) {
       console.log("there is no post", post);
@@ -58,7 +51,7 @@ export default function BlogPost() {
 
   return (
     <>
-     {post && ( <section className={styles.container}>
+      {post && (<section className={styles.container}>
         <Heading>{post.title}</Heading>
         {post?.image && <BlogImageBanner src={post.image} alt={post.title} />}
         <div className={styles.dateContainer}>
@@ -67,15 +60,14 @@ export default function BlogPost() {
         </div>
         <div dangerouslySetInnerHTML={{ __html: post.body }} />
         {/* <span className={styles.author}>Author: {post.author}</span> */}
-
-      { isAuthor &&  <div className={styles.buttonContainer}>
+        {isAuthor && <div className={styles.buttonContainer}>
           <Button onClick={handleDeletePost}>Delete</Button>
           <Button onClick={handleEditPost}>Edit</Button>
-        </div> 
+        </div>
         }
       </section>)}
-      <Comments postId={post.id} />    
+      <Comments postId={post.id} />
       <AddComment postId={post.id} />
-      </>
+    </>
   );
 }
