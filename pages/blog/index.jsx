c5be934947698f8a85a2b735d33a08b344045d6c
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-06-26 11:40:17 +0200

    clean pages/blog/index.jsx

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
+import useSWR from 'swr';
+
 import styles from "./blog.module.css";
 import Heading from "@components/heading";
 
-import useSWR from 'swr';
 import { getPosts, postsCacheKey } from "../../api-routes/posts";
 
 
 export default function Blog() {
   const { data: { data: posts = [] } = {}, error } = useSWR(postsCacheKey, getPosts);
-  // console.log("[slug]/index.js (single)Blog", { posts });
   return (
     <section>
       <Heading>Blog</Heading>
@@ -27,22 +27,3 @@ export default function Blog() {
     </section>
   );
 };
-
-
-
-// const mockData = [
-//   {
-//     id: "123",
-//     title: "Community-Messaging Fit",
-//     slug: "community-messaging-fit",
-//     createdAt: "2022-02-15",
-//     body: "<p>This is a good community fit!</p>",
-//   },
-//   {
-//     id: "1234",
-//     title: "Why you should use a react framework",
-//     slug: "why-you-should-use-react-framework",
-//     createdAt: "2022-02-12",
-//     body: "<p>This is a good community fit!</p>",
-//   },
-// ];
