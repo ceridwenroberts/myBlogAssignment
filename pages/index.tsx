1d7b6b2330a7e1c22a6ac99139b922b819be71a3
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-06-26 12:23:54 +0200

    clean pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,6 @@
 import Heading from "@components/heading";
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
 
-// export default function Home() {
-//   const user = useUser;
-//   return <Heading>Home</Heading>
-// }
-
-
-
-
 export default function Home() {
   const user = useUser()
   const supabaseClient = useSupabaseClient()
@@ -17,50 +9,13 @@ export default function Home() {
   <div>
       <h1>Welcome to my app</h1>
       {user ? (
-        <p> Welcome {user.email}</p>
-      ) : (
-        <p>To write post, please login</p>
+        <p> Welcome. Your contact email is {user.email}</p>
+      ) : (<>
+        <p>Read and comment? – Go ahead! </p> 
+        <p>To write post, please login.</p>
+        </>
       )}
       </div>
       
   </>
 }
-
-
-// import { useState, useEffect } from 'react'
-
-// function MyComponent() {
-//   const user = useUser()
-//   const supabaseClient = useSupabaseClient()
-//   const [data, setData] = useState()
-
-//   useEffect(() => {
-//     async function loadData() {
-//       const { data } = await supabaseClient.from('my_table').select('*')
-//       setData(data)
-//     }
-//     if (user) loadData()
-//   }, [user])
-
-//   return (
-//     <div>
-//       <h1>Welcome to my app</h1>
-//       {user ? (
-//         <button >Is user</button>
-//       ) : (
-//         <p>No user</p>
-//       )}
-//       {/* <div>
-//         {data ? (
-//           <ul>
-//             {data.map((item) => (
-//               <li key={item.id}>{item.name}</li>
-//             ))}
-//           </ul>
-//         ) : (
-//           <p>Loading data...</p>
-//         )}
-//       </div> */}
-//     </div>
-//   )
-// }
