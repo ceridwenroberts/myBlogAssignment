986b2748d4bb5208d63b5dd58f861eb3eec0cd20
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-06-26 12:16:56 +0200

    clean pages/app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,32 +21,3 @@ export default function App({ Component, pageProps }: AppProps) {
     </>
   );
 }
-
-// import "@/styles/globals.css";
-// import type { AppProps } from "next/app";
-// import RootLayout from "../components/root-layout";
-
-// import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
-// import { SessionContextProvider } from '@supabase/auth-helpers-react';
-// import { useState } from 'react';
-// import { supabase } from "../lib/supabaseClient"
-
-// export default function App({ Component, pageProps }: AppProps) {
-//     // Create a new supabase browser client on every first render.
-//     // const [supabaseClient] = useState(() => createPagesBrowserClient()); //ny client, även om det redan finns i lib/supabaseClient.js
-
-//   return (
-//     <>
-//         <SessionContextProvider
-//       // supabaseClient={supabaseClient}
-//       supabaseClient={supabase}
-//       initialSession={pageProps.initialSession}
-//     >
-//       <RootLayout>
-//         <Component {...pageProps} />
-//       </RootLayout>
-//       <div id="root" />
-//       </SessionContextProvider>
-//     </>
-//   );
-// }
